bb5082c9a98872098335c1e5b4623d97c8713af5
Nidelson Gimenez <nidelson@gmail.com>
2023-04-30 22:42:10 -0300

    feat(safearea): use safeAreaProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar, Text } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context';
 
 export function App(): JSX.Element {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <View className="flex-1 items-center justify-center bg-slate-100 dark:bg-slate-900">
-        <StatusBar style="auto" />
-        <Text className="text-4xl font-bold text-gray-700 dark:text-white">
-          Welcome to NBA
-        </Text>
-      </View>
+      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+        <SafeAreaView className="flex-1 border-blue-700 justify-center bg-slate-100 dark:bg-slate-900">
+          <StatusBar style="auto" />
+          <Text className="text-4xl text-center font-bold text-gray-700 dark:text-white">
+            Welcome to NBA
+          </Text>
+        </SafeAreaView>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
